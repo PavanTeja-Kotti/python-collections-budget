c67858138e9fb0ff9da7d4c234a0173fc250adc6
pavan teja <pavanteja85@gmail.com>
2022-03-20 17:10:05 +0530

    second module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,7 +7,6 @@ spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
-
 spending_counter = collections.Counter(spending_categories) 
 
 top5 = spending_counter.most_common(5)
